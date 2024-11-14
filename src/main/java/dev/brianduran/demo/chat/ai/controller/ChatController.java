d20788d24c2987a2cbf680f8e7d009055d980349
Brian Durán <b.duran@accenture.com>
2024-11-13 23:57:11 -0300

    Running llama3.2:1b from a container :white_check_mark:

---
+++ src/main/java/dev/brianduran/demo/chat/ai/controller/ChatController.java
@@ -0,0 +1,26 @@
+package dev.brianduran.demo.chat.ai.controller;
+
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class ChatController {
+
+    private final OllamaChatModel chatModel;
+
+    @Autowired
+    public ChatController(OllamaChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/ai/generate")
+    public Map<String,String> generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+}
